588e54274bbe5c132ff84ef14717110a382a5b0b
Felipe <felipe_oliveira2019@hotmail.com>
2023-02-10 13:56:49 -0300

    adicionando responsividade no crew

--- space-tourism-website-main/starter-code/js/crew_js/insertInfo.js
+++ space-tourism-website-main/starter-code/js/crew_js/insertInfo.js
@@ -1,37 +1,48 @@
 class insertInfo {
-    constructor(radio) {
+    constructor(radio, number, img) {
         this.radio = document.querySelectorAll(radio)
+        this.number = document.querySelector(number)
+        this.image = document.querySelector(img)
     }
+
     call = () => {
         this.radio.forEach((value, index) => {
             value.addEventListener('click', () => {
                 this.bringInfo(index)
+                this.number.innerText = index
             })
         })
     }
+
     setInfo = (value, valores) => {
         if (document.querySelector(`.${value}`)) {
             document.querySelector(`.${value}`).innerText = valores
         }
     }
+
+    setImg = (value) => {
+        this.image.setAttribute('src', `.${value}`)
+    }
+
     bringInfo = async(index) => {
         const data = await fetch('../data.json')
         const data_json = await data.json()
         const crew = data_json.crew
         const person = crew[index]
-        console.log(person)
         for (let value in person) {
-            console.log(value)
             this.setInfo(value, person[value])
         }
+        this.setImg(person.images.png)
+
     }
 
 
     init = () => {
         if (this.radio) {
             this.call()
+            this.bringInfo(0)
         }
     }
 }
-const setInfo = new insertInfo('.crew')
+const setInfo = new insertInfo('.crew', '.number', '.images')
 setInfo.init()
