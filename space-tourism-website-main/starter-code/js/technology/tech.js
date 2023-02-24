bdf2e45144ad01ad7891135f87374ce0eeac9cc5
Felipe <felipe_oliveira2019@hotmail.com>
2023-02-24 14:39:12 -0300

    finalizando o projeto

--- space-tourism-website-main/starter-code/js/technology/tech.js
+++ space-tourism-website-main/starter-code/js/technology/tech.js
@@ -1,20 +1,22 @@
 const buttons = document.querySelectorAll('.button')
 const image = document.querySelector('.images')
 const id = document.querySelector(".id")
-let PageWidth = window.screen.width
 const getInfo = async() => {
     const infoTech = await fetch('../data.json')
     const TechJson = await infoTech.json()
     const technology = TechJson.technology
-
+    let num = 0
     buttons.forEach((value, index) => {
         value.addEventListener('click', () => {
             insertInfo(index)
             getImage(index)
+            num = index
             id.innerText = `0${index + 1}`
-
         })
     })
+
+    // função que insere as informações na aplicação
+
     const insertInfo = (index) => {
         const InfoRequest = technology[index]
         for (let data in InfoRequest) {
@@ -22,15 +24,21 @@ const getInfo = async() => {
         }
     }
 
-    const getImage = (index) => {
-        console.log(PageWidth)
-        const ImageRequest = technology[index].images
-        PageWidth <= 1250 ? image.setAttribute("src", `.${ImageRequest.landscape}`) : image.setAttribute('src', `.${ImageRequest.portrait}`)
+    // função que insere e ajusta a dimensão da imagem na aplicação
 
+
+    const getImage = (index) => {
+        ImageRequest = technology[index].images
+        const PageWidth = window.screen.width
+        PageWidth <= 1250 ? image.setAttribute("src", `.${ImageRequest.landscape}`) : image.setAttribute('src', `.${ImageRequest.portrait}`)
     }
+
     window.addEventListener('resize', () => {
-        PageWidth = window.screen.width
+        let ImageRequest = technology[num].images
+        const PageWidth = window.screen.width
+        PageWidth <= 1250 ? image.setAttribute("src", `.${ImageRequest.landscape}`) : image.setAttribute('src', `.${ImageRequest.portrait}`)
     })
+
 
 
     insertInfo(0)
